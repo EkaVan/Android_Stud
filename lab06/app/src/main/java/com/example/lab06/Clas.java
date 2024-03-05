f94c98e3c392ebe871deb2bc769c6401fa77fefa
EkaVan <158572557+EkaVan@users.noreply.github.com>
2024-03-05 23:51:44 +0300

    Add files via upload

---
+++ lab06/app/src/main/java/com/example/lab06/Clas.java
@@ -0,0 +1,13 @@
+package com.example.lab06;
+//package com.example.lab06;
+
+public class Clas {
+    public String title;
+    public String text;
+
+    public String toString()
+    {
+        return title;
+    }
+}
+
