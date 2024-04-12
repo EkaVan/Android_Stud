bb289de2221af0bb0581ab9428deb5c5d1561e9f
EkaVan <158572557+EkaVan@users.noreply.github.com>
2024-04-12 11:27:16 +0300

    Update MainActivity.java

--- lab_13/app/src/main/java/vanina/example/lab_13/MainActivity.java
+++ lab_13/app/src/main/java/vanina/example/lab_13/MainActivity.java
@@ -9,13 +9,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-TextView tv;
+TextView txt_res;
 
 @Override
 protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    tv = (TextView) findViewById(R.id.txt_res);
+    txt_res = findViewById(R.id.txt_res);
 }
 
 public void on_button3_click(View v)
